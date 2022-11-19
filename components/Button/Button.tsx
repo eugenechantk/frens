66ac69acfa620281d9ed37f6603362aaa72b0fab
Eugene Chan <me@eugenechantk.me>
2022-11-19 01:20:40 -0300

    Implemented button component + story for button component

---
+++ components/Button/Button.tsx
@@ -0,0 +1,167 @@
+import clsx from 'clsx';
+import React from 'react'
+
+interface ButtonProps {
+  type?: 'primary' | 'secondary' | 'outline' | 'secondary-outline' | 'text-only' | 'ghost';
+  disabled?: boolean;
+  size?: 'sm' | 'base' | 'lg';
+  className?: string;
+  onClick?: () => void;
+  children?: React.ReactNode;
+}
+
+export const Button = ({
+  type='primary',
+  size = 'base',
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      type="button"
+      className={clsx(
+        type === 'primary' && primaryStyle,
+        type === 'secondary' && secondaryStyle,
+        type === 'outline' && outlinedStyle,
+        type === 'secondary-outline' && secOutlinedStyle,
+        type === 'ghost' && ghostStyle,
+        type === 'text-only' && textOnlyStyle,
+        size === 'sm' && (type === 'text-only' ? textOnlySmSizing : smSizing) ,
+        size === 'base' && (type === 'text-only' ? textOnlyBaseSizing : baseSizing),
+        size === 'lg' && (type === 'text-only' ? textOnlyLgSizing : lgSizing),
+        props.className,
+      )}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+// ======= //
+// Component variant styling //
+// ======= //
+
+const primaryStyle = clsx(
+  // default styling
+  'bg-primary-600 border border-primary-800 text-white',
+  // hover state styling
+  'hover:bg-primary-800',
+  // disabled state styling
+  'disabled:bg-primary-200 disabled:text-primary-400 disabled:border-0 disabled:pointer-events-none'
+);
+
+const secondaryStyle = clsx(
+  // default styling
+  'bg-secondary-200 border border-secondary-300 text-secondary-600',
+  // hover state styling
+  'hover:bg-secondary-300',
+  // disabled state styling
+  'disabled:bg-gray-200 disabled:text-gray-400 disabled:border-0 disabled:pointer-events-none'
+);
+
+const outlinedStyle = clsx(
+  // default styling
+  'border-2 border-primary-600 text-primary-600',
+  // hover state styling
+  'hover:bg-primary-200',
+  // disabled state styling
+  'disabled:bg-transparent disabled:text-primary-400 disabled:border-2 disabled:border-primary-300 disabled:pointer-events-none'
+);
+
+const secOutlinedStyle = clsx(
+  // default styling
+  'border border-secondary-300 text-secondary-600',
+  // hover state styling
+  'hover:bg-secondary-200',
+  // disabled state styling
+  'disabled:bg-transparent disabled:text-gray-400 disabled:border disabled:border-gray-200 disabled:pointer-events-none'
+);
+
+const ghostStyle = clsx(
+  // default styling
+  'text-secondary-600 border-none',
+  // hover state styling
+  'hover:bg-secondary-300',
+  // disabled state styling
+  'disabled:bg-transparent disabled:text-gray-400 disabled:pointer-events-none'
+);
+
+const textOnlyStyle = clsx(
+  // default styling
+  'text-gray-800 border-none',
+  // hover state styling
+  'hover:bg-gray-200 rounded-8',
+  // disabled state styling
+  'disabled:bg-transparent disabled:text-gray-300 disabled:pointer-events-none'
+);
+
+const flex = clsx(
+  'flex flex-row justify-center items-center gap-1'
+)
+
+const baseSizing = clsx(
+  // font size
+  'text-base font-bold',
+  // padding
+  'p-3',
+  // flex
+  flex,
+  // border-radius
+  'rounded-full'
+)
+
+const smSizing = clsx(
+  // font size
+  'text-sm font-bold',
+  // padding
+  'p-3',
+  // flex-gap
+  flex,
+  // border-radius
+  'rounded-full'
+)
+
+const lgSizing = clsx(
+  // font size
+  'text-xl font-bold',
+  // padding
+  'p-3',
+  // flex-gap
+  flex,
+  // border-radius
+  'rounded-full'
+)
+
+const textOnlySmSizing = clsx(
+  // font size
+  'text-sm',
+  // padding
+  'p-1',
+  // flex-gap
+  flex,
+  // border-radius
+  'rounded-8'
+)
+
+const textOnlyBaseSizing = clsx(
+  // font size
+  'text-base',
+  // padding
+  'p-1',
+  // flex-gap
+  flex,
+  // border-radius
+  'rounded-8'
+)
+
+const textOnlyLgSizing = clsx(
+  // font size
+  'text-lg',
+  // padding
+  'p-1',
+  // flex-gap
+  flex,
+  // border-radius
+  'rounded-8'
+)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,36 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { Button } from "../components/Button/Button";
+import { db } from "../firebase/initFirebase";
+import {ArrowDownIcon, UserIcon} from '@heroicons/react/20/solid'
 
 const Home: NextPage = () => {
+  const sendData = async () => {
+    const data = {
+      display_name: "Jane Doe",
+      profile_photo: "",
+      wallet_address: "0xfc69FE666D5E1FB8374151c11Feb058300FfDCb5",
+    };
+    try {
+      const docRef = await addDoc(collection(db, "users"), data);
+      console.log("Data added: ", docRef.id);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const findUser = async (walletAddress: string) => {
+    const userRef = collection(db, "users");
+    const q = query(userRef, where("wallet_address", "==", walletAddress));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+  };
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -14,10 +42,26 @@ const Home: NextPage = () => {
         <h1 className="text-6xl font-bold text-primary-600">
           Welcome to frens
         </h1>
-        <div className='p-10 bg-green-400 rounded-8'></div>
+        <button
+          className=" px-4 py-2 rounded-full bg-primary-600 text-white m-4"
+          onClick={sendData}
+        >
+          Add data
+        </button>
+        <button
+          className=" px-4 py-2 rounded-full bg-secondary-200 border-secondary-300 border text-secondary-600 m-4"
+          onClick={() => findUser("0xfc69FE666D5E1FB8374151c11Feb058300FfDCb5")}
+        >
+          Find user
+        </button>
+        <Button type="secondary-outline" className=" w-52">
+          <UserIcon className="w-5"/>
+          <h6>Hi</h6>
+          <ArrowDownIcon className="w-5"/>
+        </Button>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
