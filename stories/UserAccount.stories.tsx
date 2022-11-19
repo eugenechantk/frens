8e1f28a1c09e92e44b437278c25dcb0be3617412
Eugene Chan <me@eugenechantk.me>
2022-11-19 15:16:54 -0300

    Added stories for account button and user account module for testing

---
+++ stories/UserAccount.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import AccountButton from "../components/UserAccount/AccountButton";
+import UserAccount from "../components/UserAccount/UserAccount";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Components/User Account",
+  component: UserAccount,
+} as ComponentMeta<typeof UserAccount>;
+
+export const Primary: ComponentStory<typeof UserAccount> = (args) => (
+  <UserAccount {...args}/>
+);
