f5707c3f89429d7aa4fdb442b21e6da7bbf3c0fc
Eugene Chan <me@eugenechantk.me>
2022-11-19 22:53:01 -0300

    Implemented getServerSideProp with firebase auth on /dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,43 @@
+import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import React, { ReactElement } from "react";
 import DashboardLayout from "../layout/DashboardLayout";
 import { NextPageWithLayout } from "./_app";
+import nookies from "nookies";
+import { getAuth } from "firebase/auth";
+import { firebaseClient } from "../firebase/initFirebase";
+import { firebaseAdmin } from "../firebase/firebaseAdmin";
 
-const Dashboard: NextPageWithLayout = () => {
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  try {
+    const cookies = nookies.get(ctx);
+    const token = await firebaseAdmin.auth().verifyIdToken(cookies.token);
+
+    // the user is authenticated!
+    const { uid, email } = token;
+
+    // FETCH STUFF HERE!! 🚀
+
+    return {
+      props: { authed: true },
+    };
+  } catch (err) {
+    // either the `token` cookie didn't exist
+    // or token verification failed
+    // either way: redirect to the login page
+
+    // return false for authed props
+    return { props: { authed: false }};
+  }
+}
+
+const Dashboard: NextPageWithLayout<InferGetServerSidePropsType<typeof getServerSideProps>> = (props) => {
   return (
     <>
-      <p>Dashboard</p>
+      {props.authed ? (
+        <p>Logged in</p>
+      ) : (
+        <p>Not logged in</p>
+      )}
     </>
   );
 };
