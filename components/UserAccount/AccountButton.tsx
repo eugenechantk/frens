8c1375fb7818bd45e047b2d339da98bfd1612009
Eugene Chan <me@eugenechantk.me>
2022-11-20 00:26:31 -0300

    added handleLogin function with magic connect for AccountButton

--- components/UserAccount/AccountButton.tsx
+++ components/UserAccount/AccountButton.tsx
@@ -1,26 +1,48 @@
-import Image from 'next/image';
-import React from 'react'
-import {Button} from '../Button/Button'
-import devProfilePic from '../../public/user_avatar.png'
-import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import Image from "next/image";
+import React from "react";
+import { Button } from "../Button/Button";
+import devProfilePic from "../../public/user_avatar.png";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { provider } from "../../lib/provider";
+import { magic } from "../../lib/magic";
 
 interface IAccountButtonProps {
   authed?: boolean;
-  onClick?: () => void;
 }
 
-export default function AccountButton({authed, onClick, ...props}: IAccountButtonProps) {
+export default function AccountButton({
+  authed,
+  ...props
+}: IAccountButtonProps) {
+  const handleLogin = async () => {
+    await provider!.send("eth_accounts", []);
+  };
+
+  const handleLogout = async () => {
+    await magic?.connect.disconnect().catch((e) => console.log(e));
+  }
   return (
-    <Button type='secondary-outline' className='w-[70px] h-[38px] px-1' onClick={onClick}>
-      {!authed ? (
-        <h6>Log in</h6>
-      ) : (
-        <>
-          <Image src={devProfilePic} alt="User Profile" width={30} height={30}/>
-          <ChevronDownIcon className='w-5 mr-2'/>
-        </>
-        
-      )}
-    </Button>
-  )
+    <>
+      <Button
+        type="secondary-outline"
+        className="w-[70px] h-[38px] px-1"
+        onClick={handleLogin}
+      >
+        {!authed ? (
+          <h6>Log in</h6>
+        ) : (
+          <>
+            <Image
+              src={devProfilePic}
+              alt="User Profile"
+              width={30}
+              height={30}
+            />
+            <ChevronDownIcon className="w-5 mr-2" />
+          </>
+        )}
+      </Button>
+      <button onClick={handleLogout}>Log out</button>
+    </>
+  );
 }
