6410af3d90c0aebb292dcbea318fe06f3eedcd36
Eugene Chan <me@eugenechantk.me>
2022-11-21 00:13:03 -0300

    Removed getServerSideProps from dashboard (cuz not using it now)

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -8,29 +8,6 @@ import { firebaseClient } from "../firebase/firebaseClient";
 import { firebaseAdmin } from "../firebase/firebaseAdmin";
 import { useAuth } from "../lib/auth/auth";
 
-export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  try {
-    const cookies = nookies.get(ctx);
-    const token = await firebaseAdmin.auth().verifyIdToken(cookies.token);
-
-    // the user is authenticated!
-    const { uid, email } = token;
-
-    // FETCH STUFF HERE!! 🚀
-
-    return {
-      props: { authed: true },
-    };
-  } catch (err) {
-    // either the `token` cookie didn't exist
-    // or token verification failed
-    // either way: redirect to the login page
-
-    // return false for authed props
-    return { props: { authed: false }};
-  }
-}
-
 const Dashboard: NextPageWithLayout<any> = () => {
   const user = useAuth()
   return (
