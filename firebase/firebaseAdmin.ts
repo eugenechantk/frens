ac34edf306edc5748f44a40e161bf931409a7b2f
Eugene Chan <me@eugenechantk.me>
2022-11-21 11:28:53 -0300

    Enable firebaseClient and firebaseAdmin to connect to emulator only in dev mode

--- firebase/firebaseAdmin.ts
+++ firebase/firebaseAdmin.ts
@@ -1,13 +1,20 @@
-import * as firebaseAdmin from 'firebase-admin';
+import * as firebaseAdmin from "firebase-admin";
 
 // get this JSON from the Firebase board
 // you can also store the values in environment variables
 const serviceAccount = JSON.parse(
   process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SECRET as string
-)
+);
+
+const id =
+  process.env.NEXT_PUBLIC_NODE_ENV === "development"
+    ? `demo-${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}`
+    : process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+
 
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
+    projectId: id,
     credential: firebaseAdmin.credential.cert({
       privateKey: serviceAccount.private_key,
       clientEmail: serviceAccount.client_email,
--- firebase/firebaseClient.ts
+++ firebase/firebaseClient.ts
@@ -9,34 +9,27 @@ import 'firebase/storage'
 import 'firebase/analytics'
 import 'firebase/performance'
 import { browserSessionPersistence, getAuth, connectAuthEmulator, Auth } from "firebase/auth";
-import * as firebase from 'firebase/app'
+
+const id = process.env.NEXT_PUBLIC_NODE_ENV === 'development' ? `demo-${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}`: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  projectId: id,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-async function setupEmulator(auth: Auth) {
-  const authUrl = "http://127.0.0.1:9099";
-  await fetch(authUrl);
-  connectAuthEmulator(firebaseClientAuth, authUrl);
-}
-
 export const firebaseClient = initializeApp(firebaseConfig);
-export const db = getFirestore(firebaseClient);
-
 // Initialize Firebase Authentication and get a reference to the service
 export const firebaseClientAuth = getAuth(firebaseClient);
-setupEmulator(firebaseClientAuth);
 // Only setup emulator in development mode
-// if (process.env.NEXT_PUBLIC_NODE_ENV === 'local') {
-//   setupEmulator(firebaseClientAuth);
-// }
+if (process.env.NEXT_PUBLIC_NODE_ENV === 'development' && !firebaseClientAuth.emulatorConfig) {
+  connectAuthEmulator(firebaseClientAuth, "http://127.0.0.1:9099");
+}
 firebaseClientAuth.setPersistence(browserSessionPersistence);
+export const db = getFirestore(firebaseClient);
 
