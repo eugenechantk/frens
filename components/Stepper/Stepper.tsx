3180e400fad67ee5b865e2a1765ae209a7ec4d32
Eugene Chan <me@eugenechantk.me>
2022-11-29 19:45:21 -0300

    Implemented stepper component FE

--- components/Stepper/Stepper.tsx
+++ components/Stepper/Stepper.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-export default function Stepper({children}:{children: React.ReactNode}) {
+interface IStepperProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function Stepper({children, className}:IStepperProps) {
+  const arrayChildren = React.Children.toArray(children);
   return (
-    <div className='flex flex-row items-center'>
-      {children}
+    <div className='flex flex-row items-center w-full'>
+      {React.Children.map(arrayChildren, (children, index) => {
+        return (
+          <>
+            {children}
+            {index !== arrayChildren.length - 1 && <div className=' h-[3px] rounded-full bg-secondary-300 mx-2 grow'></div>}
+          </>
+        )
+      })}
     </div>
   )
 }
