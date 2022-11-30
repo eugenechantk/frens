72a43294509f3a9115d8e048029ba15dcc91fe0d
Eugene Chan <me@eugenechantk.me>
2022-11-29 23:31:58 -0300

    Refactored the app layout

--- layout/AppLayout.tsx
+++ layout/AppLayout.tsx
@@ -5,7 +5,7 @@ export default function AppLayout({children}: {children: React.ReactNode}) {
   return (
     <div className='h-full w-full flex flex-col'>
       <NavBar/>
-      <main className='w-full grow'>{children}</main>
+      <main className='grow'>{children}</main>
     </div>
   )
 }
