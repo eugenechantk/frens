12fc37b2ca34408573b19f17ef2ba675b61c486f
Eugene Chan <me@eugenechantk.me>
2022-11-30 15:20:00 -0300

    Used next/font to host my Montserrat Google Font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import type { NextPage } from 'next'
-import { AuthProvider } from '../lib/auth'
-import { Montserrat } from '@next/font/google'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import type { NextPage } from "next";
+import { AuthProvider } from "../lib/auth";
+import { Montserrat } from "@next/font/google";
 
 export type NextPageWithLayout<Props> = NextPage<Props> & {
-  getLayout?: (page: React.ReactElement) => React.ReactNode
-}
+  getLayout?: (page: React.ReactElement) => React.ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout<any>
-}
+  Component: NextPageWithLayout<any>;
+};
 
-const montserrat = Montserrat({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page)
+  const getLayout = Component.getLayout ?? ((page) => page);
   return (
-    <AuthProvider className={montserrat.className}>
-      {getLayout(<Component {...pageProps} />)}
+    <AuthProvider>
+      {getLayout(
+        <>
+          <style jsx global>{`
+            html {
+              font-family: ${montserrat.style.fontFamily};
+            }
+          `}</style>
+          <Component {...pageProps} />
+        </>
+      )}
     </AuthProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
