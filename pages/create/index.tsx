e1681120f881bb4178985c82fffae1230eb1b86d
Eugene Chan <me@eugenechantk.me>
2022-11-30 11:33:55 -0300

    Implemented create page

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -24,64 +24,79 @@ const CreateClub: NextPageWithLayout<any> = () => {
       className=" flex flex-col gap-10 w-full justify-center"
     >
       {/* Club information */}
-      <div className="flex flex-col items-start gap-4">
+      <div className="flex flex-col items-start gap-4 w-full">
         <p className="font-bold leading-5 text-sm text-secondary-600 uppercase">
           Club information
         </p>
-        {/* Image upload field */}
-        <div className="flex flex-col items-start gap-2">
-          <p className="text-sm text-gray-800 font-semibold leading-5">
-            Profile photo
-          </p>
-          <ImageUpload width={64} />
+        <div className="md:flex md:flex-row md:items-start md:gap-8 flex flex-col items-start gap-4 w-full">
+          {/* Image upload field */}
+          <div className="flex flex-col items-start gap-2 w-[94px] md:min-w-[94px]">
+            <p className="text-sm text-gray-800 font-semibold leading-5 md:grow">
+              Profile photo
+            </p>
+            <ImageUpload />
+          </div>
+          <div className="md:flex md:flex-col md:items-start md:gap-4 md:shrink md:min-w-0 flex flex-col items-start gap-4 w-full">
+            <InputField
+              name="clubName"
+              label="Club name"
+              placeholder="e.g. Friends with profits"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setClubName(e.target.value)
+              }
+            />
+            <InputField
+              name="clubDesc"
+              type="text-area"
+              label="Club description"
+              placeholder="Something about your investment club"
+            />
+            <InputField
+              name="tokenSym"
+              label="Club token symbol"
+              description="Each member will receive club tokens to represent their ownership of the club."
+              defaultValue={clubName && clubName.slice(0, 6).toUpperCase()}
+              className="uppercase"
+            />
+          </div>
         </div>
-        <InputField
-          name="clubName"
-          label="Club name"
-          placeholder="e.g. Friends with profits"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setClubName(e.target.value)
-          }
-        />
-        <InputField
-          name="clubDesc"
-          type="text-area"
-          label="Club description"
-          placeholder="Something about your investment club"
-        />
-        <InputField
-          name="tokenSym"
-          label="Club token symbol"
-          description="Each member will receive club tokens to represent their ownership of the club."
-          defaultValue={clubName && clubName.slice(0, 6).toUpperCase()}
-          className="uppercase"
-        />
       </div>
       {/* Creation fee */}
       <div className="flex flex-col items-start">
         <p className="font-bold leading-5 text-sm text-secondary-600 uppercase mb-1">
           Creation fee
         </p>
-        <p className=" text-base leading-6 text-gray-800 mb-4">The creation fee is to set up the club token and enables the club to raise money</p>
-        
+        <p className=" text-base leading-6 text-gray-800 mb-4">
+          The creation fee is to set up the club token and enables the club to
+          raise money
+        </p>
+
         {/* Creation fee estimate box */}
         <div className="flex flex-row items-start px-4 py-3 gap-4 bg-primary-200 border border-primary-300 rounded-8 w-full">
           {/* Estimated fee icons and title */}
           <div className="flex flex-row items-center gap-2 grow">
-            <WrenchScrewdriverIcon className="w-6 text-primary-800"/>
-            <p className="text-base leading-6 font-semibold text-primary-800">Estimated fees</p>
+            <WrenchScrewdriverIcon className="w-6 text-primary-800" />
+            <p className="text-base leading-6 font-semibold text-primary-800">
+              Estimated fees
+            </p>
           </div>
           {/* Fee estimate display */}
           <div className="flex flex-row gap-2">
             {/* TODO: render the estimated ETH price and USD equivalent */}
-            <p className="text-primary-800 text-base leading-6 font-semibold">0.0756 ETH</p>
-            <p className="text-primary-500 text-base leading-6 font-semibold">12.37 USD</p>
+            <p className="text-primary-800 text-base leading-6 font-semibold">
+              0.0756 ETH
+            </p>
+            <p className="text-primary-500 text-base leading-6 font-semibold">
+              12.37 USD
+            </p>
           </div>
         </div>
       </div>
       {/* Create button */}
       <div className="flex flex-col items-center gap-3">
-        <p className="text-sm leading-5 text-gray-500">By clicking “Pay and create” you agree to our Terms of Service</p>
+        <p className="text-sm leading-5 text-gray-500">
+          By clicking “Pay and create” you agree to our Terms of Service
+        </p>
         <Button type="submit" className="w-1/3 min-w-[218px]">
           Pay and create
         </Button>
