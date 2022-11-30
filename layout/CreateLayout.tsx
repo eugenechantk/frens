3aa307292a49c806370d1aed5a97f1dccb748df9
Eugene Chan <me@eugenechantk.me>
2022-11-30 11:33:39 -0300

    fixed header in CreateLayout

--- layout/CreateLayout.tsx
+++ layout/CreateLayout.tsx
@@ -20,23 +20,28 @@ export default function CreateLayout({
       <div
         className="
         h-full
-        md:flex md:flex-col md:p-6 md:items-start md:gap-6
+        md:flex md:flex-col md:items-start md:gap-6
         md:border md:border-secondary-300 md:rounded-20 md:w-[620px]
         md:overflow-auto md:my-12
-        flex flex-col items-start pt-3 pb-6 px-4 gap-6"
+        flex flex-col items-start gap-6"
       >
         {/* section header */}
         <div className="
           flex
           md:flex-row md:items-center md:gap-4 md:w-full
-          flex-col items-start gap-4"
+          flex-col items-start gap-4
+          md:pb-4 md:sticky md:top-0 md:bg-secondary-100 md:p-6
+          p-4 z-20"
         >
-          <h3 className=" leading-8 font-bold md:order-1 order-2 grow">Create your club</h3>
+          <h3 className=" leading-8 font-bold md:order-1 order-2 grow ">Create your club</h3>
           <Button variant="secondary-outline" className="md:order-2 order-1" onClick={() => router.push('/dashboard')}>
             <XMarkIcon className="w-5"/>
           </Button>
         </div>
-        {children}
+        <div className="md:px-6 md:pb-6 px-4 pb-6">
+          {children}
+        </div>
+        
       </div>
     </div>
   );
