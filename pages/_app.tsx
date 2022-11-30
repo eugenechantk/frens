d7556b787ffcc438ca2409678365029d8800d088
Eugene Chan <me@eugenechantk.me>
2022-11-30 15:16:47 -0300

    Used next/font to host my Montserrat Google Font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import type { NextPage } from 'next'
 import { AuthProvider } from '../lib/auth'
+import { Montserrat } from '@next/font/google'
 
 export type NextPageWithLayout<Props> = NextPage<Props> & {
   getLayout?: (page: React.ReactElement) => React.ReactNode
@@ -11,10 +12,12 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout<any>
 }
 
+const montserrat = Montserrat({ subsets: ['latin'] })
+
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
   return (
-    <AuthProvider>
+    <AuthProvider className={montserrat.className}>
       {getLayout(<Component {...pageProps} />)}
     </AuthProvider>
   );
