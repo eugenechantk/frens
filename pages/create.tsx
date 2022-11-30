654ae6c2abf5ca557953273ec8a611eda7fa3ada
Eugene Chan <me@eugenechantk.me>
2022-11-29 23:32:18 -0300

    Implemented the create route

---
+++ pages/create.tsx
@@ -0,0 +1,20 @@
+import React, { ReactElement } from "react";
+import { NextPageWithLayout } from "./_app";
+import AppLayout from "../layout/AppLayout";
+import CreateLayout from "../layout/CreateLayout";
+
+const CreateClub: NextPageWithLayout<any> = () => {
+  return <>Create club</>;
+};
+
+CreateClub.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <AppLayout>
+      <CreateLayout>
+        {page}
+      </CreateLayout>
+    </AppLayout>
+  );
+};
+
+export default CreateClub;
