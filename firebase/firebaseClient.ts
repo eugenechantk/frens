e684894ff08207f7e04ba02743421ce10dcfeaac
Eugene Chan <me@eugenechantk.me>
2022-12-03 20:46:32 -0300

    removed firebase emulator setup for now

--- firebase/firebaseClient.ts
+++ firebase/firebaseClient.ts
@@ -2,7 +2,7 @@
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from 'firebase/app'
 import { getPerformance } from "firebase/performance";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import 'firebase/auth'
 import 'firebase/firestore'
@@ -17,7 +17,7 @@ const id = process.env.NEXT_PUBLIC_NODE_ENV === 'development' ? `demo-${process.
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: id,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
@@ -25,12 +25,16 @@ const firebaseConfig = {
 };
 
 export const firebaseClient = initializeApp(firebaseConfig);
+console.log('Firebase client initialized');
 // Initialize Firebase Authentication and get a reference to the service
 export const firebaseClientAuth = getAuth(firebaseClient);
 // Only setup emulator in development mode
-if (process.env.NEXT_PUBLIC_NODE_ENV === 'development' && !firebaseClientAuth.emulatorConfig) {
-  connectAuthEmulator(firebaseClientAuth, "http://127.0.0.1:9099");
-}
+// if (process.env.NEXT_PUBLIC_NODE_ENV === 'development' && !firebaseClientAuth.emulatorConfig) {
+//   connectAuthEmulator(firebaseClientAuth, "http://127.0.0.1:9099");
+// }
 firebaseClientAuth.setPersistence(browserSessionPersistence);
 export const clientStorage = getStorage(firebaseClient);
-
+export const clientFireStore = getFirestore(firebaseClient);
+// if (process.env.NEXT_PUBLIC_NODE_ENV === 'development' && !firebaseClientAuth.emulatorConfig) {
+//   connectFirestoreEmulator(clientFireStore, '127.0.0.1', 8080);
+// }
