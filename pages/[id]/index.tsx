dd77df1c9358141701f82820443eb56f536d7dda
Eugene Chan <me@eugenechantk.me>
2022-12-07 16:05:42 -0300

    Changed dashboard file to index file under /[id] route

--- pages/[id]/index.tsx
+++ pages/[id]/index.tsx
@@ -1,14 +1,9 @@
-import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import React, { ReactElement } from "react";
-import AppLayout from "../layout/AppLayout";
-import { NextPageWithLayout } from "./_app";
-import nookies from "nookies";
-import { getAuth } from "firebase/auth";
-import { firebaseClient } from "../firebase/firebaseClient";
-import { firebaseAdmin } from "../firebase/firebaseAdmin";
-import { useAuth } from "../lib/auth";
+import AppLayout from "../../layout/AppLayout";
+import { NextPageWithLayout } from "../_app";
+import { useAuth } from "../../lib/auth";
 import { useRouter } from "next/router";
-import { Button } from "../components/Button/Button";
+import { Button } from "../../components/Button/Button";
 
 const Dashboard: NextPageWithLayout<any> = () => {
   const user = useAuth()
