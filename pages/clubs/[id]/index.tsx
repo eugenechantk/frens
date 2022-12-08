439845657caedcd0e624d1d3841a2b7282579e7d
Eugene Chan <me@eugenechantk.me>
2022-12-08 03:31:40 -0300

    Implemented serverSideProps and rendering for /clubs page

--- pages/clubs/[id]/index.tsx
+++ pages/clubs/[id]/index.tsx
@@ -3,16 +3,49 @@ import AppLayout from "../../../layout/AppLayout";
 import { NextPageWithLayout } from "../../_app";
 import { useAuth } from "../../../lib/auth";
 import { useRouter } from "next/router";
-import { Button } from "../../../components/Button/Button";
+import { firebaseAdmin } from "../../../firebase/firebaseAdmin";
+import { InferGetServerSidePropsType } from "next";
 
-const Dashboard: NextPageWithLayout<any> = () => {
-  const user = useAuth()
+export const getServerSideProps = async (context: any) => {
+  const { id } = context.params;
+  const clubInfo = await firebaseAdmin
+    .firestore()
+    .collection("clubs")
+    .doc(id)
+    .get()
+    .then((doc) => {
+      if(!doc.exists) {
+        return {
+          notFound: true,
+        };
+      }
+      return doc.data()
+    }).then((data) => {
+      return {
+        props: {
+          ...data
+        }
+      }
+    }).catch((err) => {
+      console.log(err);
+      return {
+        notFound: true,
+      }; 
+    });
+  return {
+    ...clubInfo
+  };
+};
+
+const Dashboard: NextPageWithLayout<any> = (serverProps: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  console.log(serverProps)
+  const user = useAuth();
   const router = useRouter();
   // console.log(user && user.user)
   return (
     <div className="h-full w-full py-8 px-4">
-      <p>Dashboard
-      </p>
+      <h3>{serverProps.club_name}</h3>
+      <p>{serverProps.club_description}</p>
     </div>
   );
 };
