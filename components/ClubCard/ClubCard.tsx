b73846004cd4d7f8f6aa2971fb92b5e911238859
Eugene Chan <me@eugenechantk.me>
2022-12-08 03:30:52 -0300

    Refactored how ClubCard render profile image + added onClick handler

--- components/ClubCard/ClubCard.tsx
+++ components/ClubCard/ClubCard.tsx
@@ -4,18 +4,20 @@ import React from "react";
 interface IClubCardProps {
   clubName: string;
   clubDes: string;
-  profileImgUrl: string | StaticImageData;
+  profileImgUrl: string;
+  onClick?: () => void;
 }
 
 export default function ClubCard({
   clubName,
   clubDes,
   profileImgUrl,
+  ...props
 }: IClubCardProps) {
   return (
     <div
       className="flex flex-col justify-between items-start p-6 h-[212px] bg-white rounded-20 w-full cursor-pointer border-[3px] border-white hover:border-primary-600"
-      onClick={() => console.log(`${clubName} clicked`)}
+      onClick={props.onClick}
     >
       <Image
         src={profileImgUrl}
@@ -26,7 +28,9 @@ export default function ClubCard({
       />
       <div className="flex flex-col gap-1 items-start w-full">
         <h3>{clubName}</h3>
-        <p className=" text-sm leading-5 w-full line-clamp-2">{clubDes}</p>
+        {clubDes && (
+          <p className=" text-sm leading-5 w-full line-clamp-2">{clubDes}</p>
+        )}
       </div>
     </div>
   );
