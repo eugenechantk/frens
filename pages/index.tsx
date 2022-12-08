e599d0095eae097c4890e301a971a0632dcd9e11
Eugene Chan <me@eugenechantk.me>
2022-12-08 19:28:24 -0300

    Change button on / to Link to redirect to /clubs page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,10 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { Button } from "../components/Button/Button";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
@@ -17,9 +18,11 @@ const Home: NextPage = () => {
         <h1 className="text-6xl font-bold text-primary-600 mb-8">
           Welcome to frens
         </h1>
-        <Button onClick={() => router.push('/clubs')}>
-          <h3>Get started</h3>
-        </Button>
+        <Link href="/clubs" className="w-[212px]">
+          <Button className="w-full">
+            <h3>Get started</h3>
+          </Button>
+        </Link>
       </main>
     </div>
   );
