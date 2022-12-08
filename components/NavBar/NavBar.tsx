f11e02cc489ef0cd9936122324e3e091d7161fc3
Eugene Chan <me@eugenechantk.me>
2022-12-08 18:42:59 -0300

    Added link to the logo of the navbar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -4,8 +4,10 @@ import logoSrc from "../../public/logo.png";
 import Image from "next/image";
 import UserAccount from "../UserAccount/UserAccount";
 import { useRouter } from "next/router";
+import { useAuth } from "../../lib/auth";
 
 export default function NavBar() {
+  const user = useAuth();
   const router = useRouter()
   return (
     <div className="w-full min-h-[76px] relative z-50">
@@ -19,7 +21,14 @@ export default function NavBar() {
       <Image
         src={logoSrc}
         alt="frens Logo"
-        className="absolute left-[calc(50%-40px/2+3px)] top-[calc(50%-40px/2+2px)]"
+        className="absolute left-[calc(50%-40px/2+3px)] top-[calc(50%-40px/2+2px)] cursor-pointer"
+        onClick={() => {
+          if(user.user) {
+            router.push('/clubs')
+          } else {
+            router.push('/');
+          }
+        }}
       />
       <div className="absolute top-[calc(50%-38px/2)] right-4">
         <UserAccount/>
