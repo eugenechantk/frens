fe065898961b234a5eebebc18c476de1d7d1e6b1
Eugene Chan <me@eugenechantk.me>
2022-12-13 00:22:50 +0800

    Implemented who is in the club component

--- pages/clubs/[id]/index.tsx
+++ pages/clubs/[id]/index.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 import { firebaseAdmin } from "../../../firebase/firebaseAdmin";
 import { InferGetServerSidePropsType } from "next";
 import ClubDetails from "../../../components/ClubDetails/ClubDetails";
+import ClubMembers from "../../../components/ClubMembers/ClubMembers";
 
 // export const getServerSideProps = async (context: any) => {
 //   const { id } = context.params;
@@ -45,8 +46,9 @@ const Dashboard: NextPageWithLayout<any> = () => {
   // console.log(user && user.user)
   return (
     <div className="md:max-w-[1000px] w-full md:mx-auto mx-4 mt-3 md:mt-12 mb-5 h-full md:flex md:flex-row md:items-start md:gap-6">
-      <div className="">
+      <div className="flex flex-col items-start gap-4 w-full">
         <ClubDetails />
+        <ClubMembers />
       </div>
 
       <p className="w-[376px] h-[505px] bg-gray-200 shrink-0 rounded-20 p-6">Widget area</p>
