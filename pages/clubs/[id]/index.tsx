b7a368eb4c8b2da9c670d40ec3e13d2ac0fb7c69
Eugene Chan <me@eugenechantk.me>
2022-12-13 12:50:30 +0800

    fix responsive layout for club dashboard

--- pages/clubs/[id]/index.tsx
+++ pages/clubs/[id]/index.tsx
@@ -54,7 +54,7 @@ const Dashboard: NextPageWithLayout<any> = () => {
   const router = useRouter();
   // console.log(user && user.user)
   return (
-    <div className="md:max-w-[1000px] w-full md:mx-auto px-4 pt-3 md:pt-12 pb-5 h-full md:flex md:flex-row md:items-start md:gap-6">
+    <div className="md:max-w-[1000px] w-full md:mx-auto px-4 pt-3 md:pt-12 pb-5 h-full md:flex md:flex-row md:items-start md:gap-6 flex flex-col gap-8">
       {/* Left panel */}
       <div className="flex flex-col items-start gap-8">
         {/* Club details and members */}
