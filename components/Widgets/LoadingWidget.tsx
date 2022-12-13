baaf58088bf569e3ca9fa88cabb35db162c132f5
Eugene Chan <me@eugenechantk.me>
2022-12-13 23:48:18 +0800

    Remove border on the loading component so that there is no double container of the widgets when loading

--- components/Widgets/LoadingWidget.tsx
+++ components/Widgets/LoadingWidget.tsx
@@ -3,7 +3,7 @@ import Spinner from "../Spinner/Spinner";
 
 export default function LoadingWidget() {
   return (
-    <div className="w-full md:w-[376px] min-h-[475px] border border-secondary-300 shrink-0 rounded-20 p-2 flex flex-col items-center justify-center gap-2">
+    <div className="w-full md:w-[376px] min-h-[475px] flex flex-col items-center justify-center gap-2">
       <Spinner />
       <p>Loading...</p>
     </div>
