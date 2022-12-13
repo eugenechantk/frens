2016fc07dd8911da81fe6e7813cb552c6ffb3012
Eugene Chan <me@eugenechantk.me>
2022-12-13 23:47:55 +0800

    Fix metric card layout bug

--- components/ClubBalance/ClubBalance.tsx
+++ components/ClubBalance/ClubBalance.tsx
@@ -6,8 +6,8 @@ export default function ClubBalance() {
   return (
     <div className="flex flex-col md:flex-row items-start md:items-center gap-3 md:gap-6 w-full">
           <div className="flex flex-row items-start gap-2 w-full order-2 md:order-1">
-            <AssetStatus variant="change" value={7531.21} className="grow" />
-            <AssetStatus variant="total" value={2347682.12} className="grow" />
+            <AssetStatus variant="change" value={7531.21} className="w-1/2 md:w-auto" />
+            <AssetStatus variant="total" value={2347682.12} className="w-1/2 md:grow" />
           </div>
           <div className=" order-1 md:order-2 shrink-0 w-7/12 min-w-[150px] md:w-[150px]">
             <Toggle/>
