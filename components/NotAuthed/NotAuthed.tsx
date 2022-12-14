9b141c6764d821decbee5024d40d1f6307a6622b
Eugene Chan <me@eugenechantk.me>
2022-12-14 23:10:28 +0800

    Added the not authed component for server-side auth rendering

---
+++ components/NotAuthed/NotAuthed.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotAuthed() {
+  return (
+    <div className="h-full w-full py-8 md:py-12 px-4 md:px-6">
+      <h3>Please login before continuing</h3>
+    </div>
+  );
+}
