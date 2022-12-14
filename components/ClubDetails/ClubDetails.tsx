a0f6ebf6804c242fc74a1a592d41a104a466ca50
Eugene Chan <me@eugenechantk.me>
2022-12-14 18:22:02 +0800

    render club information in the clubdetails section

--- components/ClubDetails/ClubDetails.tsx
+++ components/ClubDetails/ClubDetails.tsx
@@ -1,24 +1,25 @@
 import React from "react";
-import defaultProfile from "../../public/default_club.png";
 import Image from "next/image";
 import { Button } from "../Button/Button";
 import { Cog6ToothIcon } from "@heroicons/react/24/outline";
+import { TClubInfo } from "../../pages/clubs/[id]";
 
-export default function ClubDetails() {
+export default function ClubDetails({data}:{data: TClubInfo}) {
+  // console.log('data parsed into ClubDetails', data);
   return (
     <div className="flex flex-col items-start gap-3 w-full">
       <div className="w-[60px] md:w-[52px] h-[60px] md:h-[52px] relative rounded-10 border-2 border-secondary-300 overflow-hidden">
-        <Image src={defaultProfile} alt={`Profile image for the club`} fill />
+        <Image src={data.club_image} alt={`Profile image for the club`} fill />
       </div>
       {/* Club name and settings button */}
       <div className="flex flex-row items-center gap-4">
-        <h1>Satoshi Club</h1>
+        <h1>{data.club_name}</h1>
         <Button variant="secondary-outline">
           <Cog6ToothIcon className=" w-5"/>
         </Button>
       </div>
       {/* Club description */}
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mauris eleifend condimentum vel consectetur semper justo, dictum.</p>
+      <p>{data.club_description}</p>
     </div>
   );
 }
