4286b34782fd0506c1be48bbd89be0f5103e9841
Eugene Chan <me@eugenechantk.me>
2022-12-14 23:12:46 +0800

    Removed client-side auth rendering

--- layout/AppLayout.tsx
+++ layout/AppLayout.tsx
@@ -6,21 +6,21 @@ import { useAuth } from "../lib/auth";
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   const user = useAuth();
   const router = useRouter();
-  useEffect(() => {
-    router.replace({ pathname: router.pathname, query: router.query });
-  }, [user.user]);
+  // useEffect(() => {
+  //   router.replace({ pathname: router.pathname, query: router.query });
+  // }, [user.user]);
   return (
     <div className="h-full w-full flex flex-col">
       <NavBar />
       <main className="grow">
-        {user.user ? (
+        {/* {user.user ? (
           children
         ) : (
           <div className="h-full w-full py-8 md:py-12 px-4 md:px-6">
             <h3>Please login before continuing</h3>
           </div>
-        )}
-        {/* {children} */}
+        )} */}
+        {children}
       </main>
     </div>
   );
