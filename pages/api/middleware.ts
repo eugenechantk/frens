ff62039f9dc9cea6c0b94e191fee6561da723a14
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-01-16 16:20:37 +0800

    Added authorization middleware

---
+++ pages/api/middleware.ts
@@ -0,0 +1,30 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import { firebaseAdmin } from "../../firebase/firebaseAdmin";
+
+export function withAuth(handler:NextApiHandler) {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).end('Not authenticated. No Auth header');
+    }
+
+    const token = authHeader.split(' ')[1];
+    let decodedToken;
+    try {
+      decodedToken = await firebaseAdmin.auth().verifyIdToken(token);
+      if (!decodedToken || !decodedToken.uid)
+        return res.status(401).end('Not authenticated');
+    } catch (error:any) {
+      console.log(error.errorInfo);
+      const errorCode = error.errorInfo.code;
+      error.status = 401;
+      if (errorCode === 'auth/internal-error') {
+        error.status = 500;
+      }
+      //TODO handlle firebase admin errors in more detail
+      return res.status(error.status).json({ error: errorCode });
+    }
+
+    return handler(req, res);
+  };
+}
