52ea2a3b2d5e0c1701d405dafeecfe2dee82e1f3
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-01-21 14:34:37 +0800

    Added the deposit step to buyin widget

--- components/Widgets/BuyInWidget/BuyInWidgetWrapper.tsx
+++ components/Widgets/BuyInWidget/BuyInWidgetWrapper.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { getUsdPrice } from "../../../lib/ethereum";
+import DepositBuyIn from "./DepositBuyIn";
 import InputBuyIn from "./InputBuyIn";
 
 export default function BuyInWidgetWrapper() {
@@ -14,13 +15,14 @@ export default function BuyInWidgetWrapper() {
   return (
     <>
       <h3 className="mt-[6px] ml-3 mb-2">Invest in club assets</h3>
-      <div className="bg-white flex flex-col items-center p-3 gap-4 rounded-[16px]">
+      <div className="bg-white flex flex-col items-center p-3 gap-4 rounded-[16px] h-[408px]">
         <p>Deposit ETH into the club to gain more ownership in club tokens</p>
-        <div className="w-full">
-          <InputBuyIn onClick={(buyInEth: number) => {
+        <div className="w-full h-full">
+          {step === 1 && <InputBuyIn onClick={(buyInEth: number) => {
             setStep(2);
             setBuyIn(buyInEth);
-          }}/>
+          }}/>}
+          {step === 2 && <DepositBuyIn/>}
         </div>
       </div>
     </>
---
+++ components/Widgets/BuyInWidget/DepositBuyIn.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Button } from "../../Button/Button";
+import Spinner from "../../Spinner/Spinner";
+
+export default function DepositBuyIn() {
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState<any>(false);
+  return (
+    <div className="w-full h-full">
+      <div className="flex flex-col gap-4 items-center h-full">
+        <div className="flex flex-col gap-4 items-center justify-center grow">
+          <Spinner success={success} error={error} />
+          {error ? (
+            <>
+              <h3>Fail to deposit</h3>
+              <p className="text-center px-10 md:px-3">
+                We are unable to process your deposit. Please try again to
+                deposit fund to the club
+              </p>
+            </>
+          ) : success ? (
+            <>
+              <h3>Deposit success</h3>
+              <p className="text-center px-10 md:px-3">
+                You have successfully deposited fund to the club. Welcome
+              </p>
+            </>
+          ) : (
+            <>
+              <h3>Depositing to club...</h3>
+              <p className="text-center px-10 md:px-3">
+                If you do not receive a notification for payment, press the
+                “initiate deposit again” button below
+              </p>
+            </>
+          )}
+        </div>
+        {error ? (
+          <Button className="w-[245px]">
+            <h3>Try again</h3>
+          </Button>
+        ) : success ? (
+          <Button className="w-[245px]">
+            <h3>Done</h3>
+          </Button>
+        ) : (
+          <Button className="w-[245px]" variant="secondary">
+            <h3>Initiate deposit again</h3>
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
