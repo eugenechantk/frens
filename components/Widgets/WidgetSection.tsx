163d07050593282563aa450bd56734e67976040a
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-01-21 17:46:12 +0800

    passing the club info to buyin widget

--- components/Widgets/WidgetSection.tsx
+++ components/Widgets/WidgetSection.tsx
@@ -1,18 +1,19 @@
 import React, { lazy, useState, Suspense } from "react";
+import { IClubInfo } from "../../pages/clubs/[id]";
 import LoadingWidget from "./LoadingWidget";
 import WidgetToggle from "./WidgetToggle";
 const TradeAsset = lazy(() => import("./TradeAsset"));
 const BuyInWidgetWrapper = lazy(() => import("./BuyInWidget/BuyInWidgetWrapper"));
 
-export default function WidgetSection() {
+export default function WidgetSection({data}:{data: IClubInfo}) {
   const [selected, setSelected] = useState("invest");
   return (
     <div className="flex flex-col items-start gap-2">
       <WidgetToggle selected={selected} setSelected={setSelected} />
-      <div className="w-full md:w-[376px] border border-secondary-300 shrink-0 rounded-20 p-2 flex flex-col items-start gap-2">
+      <div className="w-full md:w-[376px] h-full border border-secondary-300 shrink-0 rounded-20 p-2 flex flex-col items-start gap-2">
         <Suspense fallback={<LoadingWidget />}>
           {selected === "invest" && <TradeAsset />}
-          {selected === "buyin" && <BuyInWidgetWrapper />}
+          {selected === "buyin" && <BuyInWidgetWrapper data={data}/>}
         </Suspense>
       </div>
     </div>
