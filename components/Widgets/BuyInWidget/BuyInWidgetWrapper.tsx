c999e36f6aec1cddf40a74686f753d4c1c5d2240
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-01-21 14:36:06 +0800

    Fix tablet screen widget width styling

--- components/Widgets/BuyInWidget/BuyInWidgetWrapper.tsx
+++ components/Widgets/BuyInWidget/BuyInWidgetWrapper.tsx
@@ -15,8 +15,8 @@ export default function BuyInWidgetWrapper() {
   return (
     <>
       <h3 className="mt-[6px] ml-3 mb-2">Invest in club assets</h3>
-      <div className="bg-white flex flex-col items-center p-3 gap-4 rounded-[16px] h-[408px]">
-        <p>Deposit ETH into the club to gain more ownership in club tokens</p>
+      <div className="bg-white flex flex-col items-center p-3 gap-4 rounded-[16px] h-[408px] w-full">
+        <p className="w-full">Deposit ETH into the club to gain more ownership in club tokens</p>
         <div className="w-full h-full">
           {step === 1 && <InputBuyIn onClick={(buyInEth: number) => {
             setStep(2);
