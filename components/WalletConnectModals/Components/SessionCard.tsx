2aa38c97cc701e2b6f658a86571655b54c9cecf8
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-01-30 02:04:45 +0800

    Implemented WalletConnect widget FE

--- components/WalletConnectModals/Components/SessionCard.tsx
+++ components/WalletConnectModals/Components/SessionCard.tsx
@@ -1,6 +1,7 @@
-import { Avatar, Button, Card, Link, Text } from "@nextui-org/react";
+import { Avatar, Button, Card, Text } from "@nextui-org/react";
 import Image from "next/image";
-import NextLink from "next/link";
+import Link from "next/link";
+
 import { truncate } from "../../../lib/HelperUtil";
 
 /**
@@ -27,15 +28,16 @@ export default function SessionCard({ logo, name, url, onDisconnect }: IProps) {
         minHeight: "70px",
         width: "100%",
       }}
+      className="!mb-0"
     >
-      <Card.Body className="md:!flex-row gap-2">
-        <div className="flex flex-row items-center gap-3 grow">
+      <Card.Body className="flex !flex-row !justify-between !gap-2 !flex-wrap">
+        <div className="flex flex-row items-center gap-3 min-w-[240px] max-w-[360px]">
           <Avatar src={logo} />
           <div className="grow">
             <Text h5>
               {name}
             </Text>
-            <Link href={url} className="grow">
+            <Link href={url!} className="grow">
               {truncate(url?.split("https://")[1] ?? "Unknown", 24)}
             </Link>
           </div>
