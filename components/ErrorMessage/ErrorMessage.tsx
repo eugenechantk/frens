8c7ebb0c14e2e4f037a406843a61254b97da7e8f
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-02-01 01:43:33 +0800

    Fix errormessage component naming error

--- components/ErrorMessage/ErrorMessage.tsx
+++ components/ErrorMessage/ErrorMessage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Error() {
+export default function ErrorMessage() {
   return (
     <div className="h-full w-full py-8 md:py-12 px-4 md:px-6">
       <h3>There is an error occured. Please try again</h3>
