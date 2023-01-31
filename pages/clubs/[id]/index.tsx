72f5ac63e48c1791ba78edfa1a1c439dca493c17
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-02-01 01:20:19 +0800

    fix left panel styling issue for club dashboard

--- pages/clubs/[id]/index.tsx
+++ pages/clubs/[id]/index.tsx
@@ -220,8 +220,8 @@ const Dashboard: NextPageWithLayout<any> = ({
         <div className="md:max-w-[1000px] w-full md:mx-auto px-4 pt-3 md:pt-12 pb-5 h-full md:flex md:flex-row md:items-start md:gap-6 flex flex-col gap-8">
           {/* Left panel */}
           <div className={clsx(
-            "flex flex-col items-start gap-8 w-full md:h-full md:justify-center",
-            serverProps.error === "user not verified" && "md:w-1/2"
+            "flex flex-col items-start gap-8 w-full md:h-full",
+            serverProps.error === "user not verified" && "md:w-1/2 md:justify-center"
           )}>
             {/* Club details and members */}
             <div className="flex flex-col items-start gap-4 w-full">
