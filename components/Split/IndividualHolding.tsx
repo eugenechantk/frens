8b993baa05e8189c6891e21e2173e0c37aec9288
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-02-06 22:45:33 +0800

    take the share and token and render individual token share of each member

--- components/Split/IndividualHolding.tsx
+++ components/Split/IndividualHolding.tsx
@@ -1,13 +1,18 @@
+import { BigNumber } from "ethers";
+import { formatUnits } from "ethers/lib/utils";
 import React from "react";
+import { IHoldingsData } from "../../lib/ethereum";
 
-export default function IndividualHolding() {
+export default function IndividualHolding({share, token}: {share: number, token: IHoldingsData}) {
+  console.log(token, share)
+  const memberBalance = BigNumber.from(token.balance).mul(BigNumber.from(share)).div(BigNumber.from(10000));
   return (
     <div className="flex flex-row gap-2">
       {/* Token name */}
-      <p className="text-gray-500 grow font-semibold text-sm">Ethereum</p>
+      <p className="text-gray-500 grow font-semibold text-sm">{token.name}</p>
       {/* Token amount */}
-      <p className=" inline-block text-gray-500 font-semibold text-sm">1,234,567</p>
-      <p className=" inline-block text-gray-400 text-sm">ETH</p>
+      <p className=" inline-block text-gray-500 font-semibold text-sm">{formatUnits(memberBalance, token.decimals).match(/^-?\d*\.?0*\d{0,5}/)}</p>
+      <p className=" inline-block text-gray-400 text-sm">{token.symbol}</p>
     </div>
   );
 }
