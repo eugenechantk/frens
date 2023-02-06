47ff6a95fa065373414b1cc0eae8e1f1a60dff25
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-02-07 02:40:40 +0800

    Render name, progress and txn link for each payout progress item

--- components/Payout/PayoutItem.tsx
+++ components/Payout/PayoutItem.tsx
@@ -1,17 +1,21 @@
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
-import React from "react";
+import Link from "next/link";
+import React, { Dispatch, SetStateAction } from "react";
+import { IHoldingsData } from "../../lib/ethereum";
 import { Button } from "../Button/Button";
-import Step from "../Stepper/Step";
+import { IOps } from "./PayoutProgressLine";
 import Status from "./Status";
 
+
+
 export default function PayoutItem({
-  title,
-  txnUrl,
+  token,
+  txnHash,
   txnStatus,
   error,
 }: {
-  title:string;
-  txnUrl?: string;
+  token:string | IHoldingsData;
+  txnHash?: string;
   txnStatus?: "pending" | "done" | "in-progress";
   error?: any;
 }) {
@@ -19,12 +23,14 @@ export default function PayoutItem({
     <div className="flex flex-row items-start mb-[26px] gap-3 h-[50px]">
       <Status status={txnStatus} error={error}/>
       <div className="flex flex-col gap-[2px] items-start">
-        <p>{title}</p>
-        {txnUrl && (
+        <p>{typeof token === "string" ? token : `Distribute ${token.name}`}</p>
+        {txnHash && (
+          <Link href={`${process.env.NEXT_PUBLIC_ETHERSCAN_DOMAIN}/tx/${txnHash}`} target="_blank">
           <Button variant="text-only" size="sm" className="opacity-60">
             <p>View reciept</p>
             <ArrowTopRightOnSquareIcon className="w-4" />
           </Button>
+          </Link>
         )}
       </div>
     </div>
