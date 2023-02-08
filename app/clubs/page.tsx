abee99d7db6a00619c247b2985cbcd5696e5b638
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-02-08 21:46:41 +0800

    fix issue with handling not-authed users for club list

--- app/clubs/page.tsx
+++ app/clubs/page.tsx
@@ -7,10 +7,11 @@ import { redis } from "../../lib/redis";
 import ClubCard from "./(ClubCard)/ClubCard";
 import { Suspense } from "react";
 import LoadingClubCard from "./(ClubCard)/LoadingClubCard";
+import NotAuthed from "../../components/NotAuthed/NotAuthed";
 
 // Get the cookies and determine auth state
 async function getAuth() {
-  let userAddress = "";
+  let userAddress = undefined;
   const token = cookies().get("token")?.value;
   if (token) {
     const decodedToken = await adminAuth.verifyIdToken(token);
@@ -34,39 +35,46 @@ async function getAllClubIds(userAddress: string) {
 
 export default async function Page() {
   const userAddress = await getAuth();
-  const clubIds = await getAllClubIds(userAddress);
+  let clubIds: string[] = []
+  if (userAddress) {
+    clubIds = await getAllClubIds(userAddress);
+  }
+  
   return (
     <>
-      <div className="h-full w-full">
-        <div className="flex flex-col gap-6 md:gap-8 max-w-[1000px] mx-auto">
-          {/* Title and create button for desktop */}
-          <div className="flex flex-row items-start justify-between w-full">
-            <h1>My clubs</h1>
-            <Link href="/create">
-              <Button className="w-[218px] hidden md:block">
-                <h3>Create new club</h3>
-              </Button>
-            </Link>
+      {!userAddress ? (
+        <NotAuthed />
+      ) : (
+        <div className="h-full w-full">
+          <div className="flex flex-col gap-6 md:gap-8 max-w-[1000px] mx-auto">
+            {/* Title and create button for desktop */}
+            <div className="flex flex-row items-start justify-between w-full">
+              <h1>My clubs</h1>
+              <Link href="/create">
+                <Button className="w-[218px] hidden md:block">
+                  <h3>Create new club</h3>
+                </Button>
+              </Link>
+            </div>
+            {/* Club cards */}
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 w-full justify-items-center">
+              {clubIds.map((id, index) => {
+                return (
+                  <Suspense fallback={<LoadingClubCard />} key={index}>
+                    {/* @ts-expect-error Server Component */}
+                    <ClubCard id={id} />
+                  </Suspense>
+                );
+              })}
+              <Link href="/create">
+                <Button className="w-[218px] md:hidden">
+                  <h3>Create new club</h3>
+                </Button>
+              </Link>
+            </div>
           </div>
-          {/* Club cards */}
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 w-full justify-items-center">
-            {clubIds.map((id, index) => {
-              return (
-                <Suspense fallback={<LoadingClubCard/>} key={index}>
-                  {/* @ts-expect-error Server Component */}
-                  <ClubCard id={id} />
-                </Suspense>
-              );
-            })}
-            <Link href="/create">
-              <Button className="w-[218px] md:hidden">
-                <h3>Create new club</h3>
-              </Button>
-            </Link>
-          </div>
-          
         </div>
-      </div>
+      )}
     </>
   );
 }
