324d7f995c3799ddf2a24db72b7c7f826892c77e
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-02-09 02:58:18 +0800

    added suspense to the login button

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,20 +1,20 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Button } from "../Button/Button";
 import logoSrc from "../../public/logo.png";
 import Image from "next/image";
-import UserAccount from "../UserAccount/UserAccount";
 import { useRouter } from "next/router";
 import { useAuth } from "../../lib/auth";
+const UserAccount = lazy(() => import("../UserAccount/UserAccount"));
 
 export default function NavBar() {
   const user = useAuth();
-  const router = useRouter()
+  const router = useRouter();
   return (
     <div className="w-full min-h-[76px] relative z-50">
       <Button
         variant="ghost"
         className="absolute top-[calc(50%-44px/2)] left-[6px]"
-        onClick={() => router.push('/clubs')}
+        onClick={() => router.push("/clubs")}
       >
         <h5>My clubs</h5>
       </Button>
@@ -23,15 +23,17 @@ export default function NavBar() {
         alt="frens Logo"
         className="absolute left-[calc(50%-40px/2+3px)] top-[calc(50%-40px/2+2px)] cursor-pointer"
         onClick={() => {
-          if(user.user) {
-            router.push('/clubs')
+          if (user.user) {
+            router.push("/clubs");
           } else {
-            router.push('/');
+            router.push("/");
           }
         }}
       />
       <div className="absolute top-[calc(50%-38px/2)] right-4">
-        <UserAccount/>
+        <Suspense fallback={<Button variant="secondary-outline" className="w-[212px]"/>}>
+          <UserAccount />
+        </Suspense>
       </div>
     </div>
   );
