80d4211855839de34ab48ed2c0e7352655de9eeb
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-02-09 03:35:34 +0800

    fix rendering other error state bug in club dashboard

--- pages/clubs/[id]/index.tsx
+++ pages/clubs/[id]/index.tsx
@@ -184,7 +184,7 @@ const Dashboard: NextPageWithLayout<any> = ({
     <>
       {serverProps.error === serverPropsError.NOT_AUTH ? (
         <NotAuthed />
-      ) : !(serverProps.error in serverPropsError) ? (
+      ) : (serverProps.error && !(serverProps.error in serverPropsError)) ? (
         <ErrorMessage />
       ) : (
         <div className="md:max-w-[1000px] w-full md:mx-auto px-4 pt-3 md:pt-12 pb-5 h-full md:flex md:flex-row md:items-start md:gap-6 flex flex-col gap-8">
