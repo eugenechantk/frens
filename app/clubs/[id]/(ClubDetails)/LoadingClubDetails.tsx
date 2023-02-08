1a64ebb1681d542228b17c88f7ab568a264967b5
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-02-08 23:28:15 +0800

    fix loading styling for club dashboard

--- app/clubs/[id]/(ClubDetails)/LoadingClubDetails.tsx
+++ app/clubs/[id]/(ClubDetails)/LoadingClubDetails.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 export default function LoadingClubDetails() {
   return (
-    <div className="flex flex-col items-start gap-3 w-full">
+    <div className="flex flex-col items-start gap-4 w-full">
       <div className="w-[60px] md:w-[52px] h-[60px] md:h-[52px] relative rounded-10 bg-gray-300">
       </div>
       {/* Club name and settings button */}
-      <div className="flex flex-row items-center gap-4">
+      <div className="flex flex-row items-center gap-3">
         <div className='w-56 h-10 bg-gray-300 rounded-8'></div>
       </div>
       {/* Club description */}
-      <div className='flex flex-col gap-2'>
-        <div className='w-full h-4 bg-gray-300 rounded-8'></div>
-        <div className='w-72 h-4 bg-gray-300 rounded-8'></div>
+      <div className='flex flex-col gap-2 w-full'>
+        <div className='w-full h-5 bg-gray-300 rounded-8'></div>
+        <div className='w-72 h-5 bg-gray-300 rounded-8'></div>
       </div>
     </div>
   )
--- app/clubs/[id]/(Widget)/LoadingWidgetSectionWrapper.tsx
+++ app/clubs/[id]/(Widget)/LoadingWidgetSectionWrapper.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function LoadingWidgetSectionWrapper() {
   return (
-    <div className='w-[527px] h-full bg-gray-300 rounded-20'></div>
+    <div className='w-[527px] h-[400px] bg-gray-300 rounded-20'></div>
   )
 }
